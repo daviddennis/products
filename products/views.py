0b7bcec0136b4adf43636991e7001e5c8b3160ac
David Dennis <daviddennis@ip-172-18-75-142.ec2.internal>
2013-04-30 23:39:59 -0400

    More functionality

--- products/views.py
+++ products/views.py
@@ -8,6 +8,7 @@ from serializers import *
 import json
 from django.db import connection
 
+
 model_dict = {
     "cars": Car,
     "furniture": Furniture,
@@ -62,7 +63,6 @@ def add_attribute(request, field_name):
     return JSONResponse(resp)
 
 
-
 @csrf_exempt
 def remove_attribute(request, field_name):
 
@@ -102,6 +102,8 @@ def generic_rest_view(request, model, model_serializer, attribute_model, pk=None
 
         elif request.method == 'PUT':
             # Update
+            put_params = json.loads(request.body)
+            save_attributes_from_params(model, model_instance, put_params, attribute_model)
             data = JSONParser().parse(request)
             serializer = model_serializer(model_instance, data=data)
             if serializer.is_valid():
@@ -118,29 +120,40 @@ def generic_rest_view(request, model, model_serializer, attribute_model, pk=None
             attribute_field_dict = {}
             if len(request.GET) > 0:
                 # Search
-                field_dict = {}                
-                for field in model._meta.fields:
-                    if field.name in request.GET:
-                        field_dict[field.name+"__icontains"] = request.GET[field.name]
+                field_dict = {}
+                field_names = [f.name for f in model._meta.fields] + ['id']
+                for field_name in request.GET:
+                    if field_name in field_names:
+                        field_dict[field_name+"__icontains"] = request.GET[field_name]
                     else:
-                        attribute_field_dict[field.name] = request.GET[field.name]
+                        attribute_field_dict[field_name] = request.GET.get(field_name)
                 if field_dict:
                     models = model.objects.filter( **field_dict )
                 else:
-                    models = []
+                    if request.GET:
+                        models = model.objects.all()
+                    else:
+                        models = []
             else:
                 # List
                 models = model.objects.all()
             serializer = model_serializer(models, many=True)
             for model_i, data in zip(models, serializer.data):
                 append_attributes_to_model(model, model_i, attribute_model, data)
-            return JSONResponse(serializer.data)
+            data = serializer.data
+            for attr_name, attr_value in attribute_field_dict.items():
+                data = [d for d in data if d.get(attr_name) == attr_value]
+            return JSONResponse(data)
+
         elif request.method == 'POST':
             # Create
             data = JSONParser().parse(request)
             serializer = model_serializer(data=data)
             if serializer.is_valid():
                 serializer.save()
+                model_instance = model.objects.get(pk=serializer.data.get('id'))
+                post_params = request.POST
+                save_attributes_from_params(model, model_instance, post_params, attribute_model)
                 return JSONResponse(serializer.data, status=201)
             else:
                 return JSONResponse(serializer.errors, status=400)
@@ -156,6 +169,20 @@ def append_attributes_to_model(model, model_instance, attribute_model, data):
         attribute_instances = attribute_model.objects.all().filter( **attribute_filters )
         for attribute_instance in attribute_instances:
             data[attribute.name] = attribute_instance.value
+
+
+def save_attributes_from_params(model, model_instance, params, attribute_model):
+    attributes = Attribute.objects.all().filter(model_type=model.__name__)
+    for field_name in params:
+        for attribute in attributes:
+            if field_name == attribute.name:
+                attr_instance_data = {
+                    model.__name__.lower(): model_instance,
+                    "attribute": attribute,
+                    "value": params[field_name]
+                }
+                attr_instance = attribute_model( **attr_instance_data )
+                attr_instance.save()
 
 
 class JSONResponse(HttpResponse):
